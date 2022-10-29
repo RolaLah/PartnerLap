f34bfe77a31e979bbd6f2888f8d2eb1646176193
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-10-29 19:07:34 +0200

    Update home.dart

--- our_lap/lib/home.dart
+++ our_lap/lib/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,7 +7,7 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _Home();
+    return const _Home();
   }
 }
 
@@ -17,21 +19,79 @@ class _Home extends StatefulWidget {
 }
 
 class __HomeState extends State<_Home> {
+  // String? role; // no radio button will be selected
+  String role = "user"; // to set default value
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red,
       appBar: AppBar(
+        elevation: 12,
         leading: const Icon(Icons.home_filled),
         title: const Text("Home Screen"),
       ),
       body: SafeArea(
-        //   child: Positioned(
-        // bottom: 1,
-        // left: 1,
-        child: Text("A"),
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 22, top: 22),
+              child: Text(
+                "Hello, Xcolor",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  backgroundColor: Colors.white,
+                  letterSpacing: 3,
+                ),
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 250)),
+            RadioListTile(
+              title: Text("User", style: radioTextStyle),
+              value: "user",
+              groupValue: role,
+              onChanged: (value) {
+                setState(() {
+                  role = value.toString();
+                });
+              },
+            ),
+            RadioListTile(
+              title: Text("Amin", style: radioTextStyle),
+              value: "admin",
+              groupValue: role,
+              onChanged: (value) {
+                setState(() {
+                  role = value.toString();
+                });
+              },
+            ),
+            TextButton.icon(
+              icon: Icon(
+                Icons.info,
+                color: Colors.white,
+              ),
+              label: Text(
+                "Info",
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                debugPrint("infoBtn Preesed");
+              },
+            )
+          ],
+        ),
       ),
-      // ),
     );
   }
 }
+
+TextStyle radioTextStyle = TextStyle(
+    fontSize: 24,
+    letterSpacing: 2,
+    backgroundColor: Colors.white,
+    fontWeight: FontWeight.w500);
