4497f614d309de55603d250edc3211e580186abf
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-10-29 21:49:05 +0200

    add Email & Pass info

--- our_lap/lib/screens.dart/info.dart
+++ our_lap/lib/screens.dart/info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:our_lap/home.dart';
 
 class Info extends StatelessWidget {
   const Info({super.key});
@@ -6,13 +7,21 @@ class Info extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 12,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.logout)),
+        title: const Text("User Info"),
+      ),
       body: Align(
         alignment: Alignment.topCenter,
         child: Column(
           children: [
-            Text("ID", style: _defTextStyle),
-            Text("Email", style: _defTextStyle),
+            Text(user!.uid.toString(), style: _defTextStyle),
+            Text(user!.email.toString(), style: _defTextStyle),
             Text("Role", style: _defTextStyle),
           ],
         ),
@@ -21,4 +30,4 @@ class Info extends StatelessWidget {
   }
 }
 
-var _defTextStyle = TextStyle(fontSize: 30);
+var _defTextStyle = TextStyle(fontSize: 22);
