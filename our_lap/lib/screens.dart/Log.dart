3f0cc107c12b9698947c80831967f512f2c33798
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-10-29 21:04:15 +0200

    clean home

--- our_lap/lib/screens.dart/Log.dart
+++ our_lap/lib/screens.dart/Log.dart
@@ -9,143 +9,172 @@ class Login_screen extends StatefulWidget {
   State<Login_screen> createState() => _Login_screenState();
 }
 
+TextEditingController emailController = TextEditingController();
+TextEditingController passController = TextEditingController();
+
 class _Login_screenState extends State<Login_screen> {
-  TextEditingController Email = TextEditingController();
-  TextEditingController Password = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                fit: BoxFit.cover, image: AssetImage("images/1.jpg"))),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Text(
-                  "Hi..",
-                  style: TextStyle(fontSize: 30, fontFamily: "AbyssinicaSIL"),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 60,
-            ),
-            TextField(
-              controller: Email,
-              textInputAction: TextInputAction.next,
-              decoration: InputDecoration(
-                label: Text(
-                  "Email :",
-                  style: TextStyle(
-                    fontFamily: "AbyssinicaSIL",
-                    fontSize: 25,
-                  ),
-                ),
-                prefixIcon: Icon(Icons.email),
-                hintText: " enter your email",
-                prefixIconColor: Color.fromARGB(255, 164, 6, 179),
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                        width: 30, color: Color.fromARGB(255, 164, 6, 179)),
-                    borderRadius: BorderRadius.all(Radius.circular(30)),
-                    gapPadding: 20),
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            TextField(
-              controller: Password,
-              textInputAction: TextInputAction.next,
-              obscureText: true,
-              decoration: InputDecoration(
-                label: Text(
-                  " Password :",
-                  style: TextStyle(
-                    fontFamily: "AbyssinicaSIL",
-                    fontSize: 25,
-                  ),
-                ),
-                prefixIcon: Icon(Icons.email),
-                hintText: "enter your password",
-                prefixIconColor: Color.fromARGB(255, 164, 6, 179),
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                        width: 30, color: Color.fromARGB(255, 164, 6, 179)),
-                    borderRadius: BorderRadius.all(Radius.circular(30)),
-                    gapPadding: 20),
-              ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextButton(
-                onPressed: () async {
-                  try {
-                    var authenticationoject = FirebaseAuth.instance;
-                    UserCredential MyUser =
-                        await authenticationoject.signInWithEmailAndPassword(
-                            email: Email.text, password: Password.text);
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text("Done successfull")));
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: ((context) {
-                      return Home();
-                    })));
-                  } catch (e) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text("Sorry, please try again")));
-                  }
-                },
-                child: Text(
-                  "Sign in",
-                  style: TextStyle(
-                    fontFamily: "AbyssinicaSIL",
-                    fontSize: 20,
-                  ),
-                )),
-            SizedBox(
-              height: 15,
-            ),
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.only(right: 150)),
-                Text(
-                  "Not a member?",
-                  style: TextStyle(fontSize: 20, fontFamily: "AbyssinicaSIL"),
-                ),
-                TextButton(
-                    onPressed: () async {
-                      try {
-                        var authenticationoject = FirebaseAuth.instance;
-                        UserCredential MyUser = await authenticationoject
-                            .createUserWithEmailAndPassword(
-                                email: Email.text, password: Password.text);
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text("Done successfull")));
-                      } catch (e) {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text(
-                                "Sorry, there already exists an account registered with this email address")));
-                      }
-                    },
-                    child: Text(
-                      "Sign Up",
-                      style:
-                          TextStyle(fontSize: 20, fontFamily: "AbyssinicaSIL"),
-                    )),
-              ],
-            )
-          ],
-        ),
+      body: ListView(
+        children: [
+          sizedBox_Def,
+          email_TextField,
+          sizedBox_Def,
+          pass_TextField,
+          sizedBox_Def,
+          SignIn(),
+          sizedBox_Def,
+          notAMember_Text,
+          SignUp(),
+          chip_Red,
+          chip_Blue,
+          chip_MixColors
+        ],
       ),
     );
+  }
+}
+
+var sizedBox_Def = SizedBox(
+  height: 40,
+);
+var email_TextField = TextField(
+  controller: emailController,
+  textInputAction: TextInputAction.next,
+  decoration: InputDecoration(
+    label: Text(
+      "Email :",
+      style: TextStyle(
+        fontFamily: "AbyssinicaSIL",
+        fontSize: 25,
+      ),
+    ),
+    prefixIcon: Icon(Icons.email),
+    hintText: " enter your email",
+    prefixIconColor: Color.fromARGB(255, 164, 6, 179),
+    border: OutlineInputBorder(
+        borderSide:
+            BorderSide(width: 30, color: Color.fromARGB(255, 164, 6, 179)),
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        gapPadding: 20),
+  ),
+);
+var pass_TextField = TextField(
+  controller: passController,
+  textInputAction: TextInputAction.next,
+  obscureText: true,
+  decoration: InputDecoration(
+    label: Text(
+      " Password :",
+      style: TextStyle(
+        fontFamily: "AbyssinicaSIL",
+        fontSize: 25,
+      ),
+    ),
+    prefixIcon: Icon(Icons.password),
+    hintText: "enter your password",
+    prefixIconColor: Color.fromARGB(255, 164, 6, 179),
+    border: OutlineInputBorder(
+        borderSide:
+            BorderSide(width: 30, color: Color.fromARGB(255, 164, 6, 179)),
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        gapPadding: 20),
+  ),
+);
+var notAMember_Text = Text(
+  "Not a member?",
+  style: TextStyle(fontSize: 20, fontFamily: "AbyssinicaSIL"),
+  textAlign: TextAlign.center,
+);
+var chip_Red = InkWell(
+  child: Chip(
+    label: Text("Red"),
+  ),
+  onTap: () {
+    emailController.text = "red@red.com";
+    passController.text = "12341234";
+  },
+);
+var chip_Blue = InkWell(
+  child: Chip(
+    label: Text("Blue"),
+  ),
+  onTap: () {
+    emailController.text = "blue@blue.com";
+    passController.text = "12341234";
+  },
+);
+var chip_MixColors = InkWell(
+  child: Chip(
+    label: Text("MixColors"),
+  ),
+  onTap: () {
+    emailController.text = "mix@colors.com";
+    passController.text = "12341234";
+  },
+);
+
+class SignUp extends StatelessWidget {
+  const SignUp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () async {
+        try {
+          var authenticationoject = FirebaseAuth.instance;
+          UserCredential MyUser =
+              await authenticationoject.createUserWithEmailAndPassword(
+                  email: emailController.text, password: passController.text);
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text("Done successfull")));
+        } catch (e) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(
+                  "Sorry, there already exists an account registered with this email address")));
+        }
+      },
+      child: Text(
+        "Sign Up",
+        style: TextStyle(fontSize: 20, fontFamily: "AbyssinicaSIL"),
+      ),
+    );
+  }
+}
+
+class SignIn extends StatelessWidget {
+  const SignIn({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: () async {
+          try {
+            var authenticationoject = FirebaseAuth.instance;
+            UserCredential MyUser =
+                await authenticationoject.signInWithEmailAndPassword(
+                    email: emailController.text, password: passController.text);
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text("Done successfull")));
+            Navigator.push(context, MaterialPageRoute(builder: ((context) {
+              return Home();
+            })));
+          } catch (e) {
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text("Sorry, please try again")));
+          }
+        },
+        child: Text(
+          "Sign in",
+          style: TextStyle(
+            fontFamily: "AbyssinicaSIL",
+            fontSize: 20,
+          ),
+        ));
   }
 }
