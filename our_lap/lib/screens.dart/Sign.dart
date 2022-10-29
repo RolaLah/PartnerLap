8b9eae65d22a93d9166c1dcabdfd858c0d56f5aa
RolaLah <rolalahloub6@gmail.com>
2022-10-29 18:43:05 +0200

    secound

---
+++ our_lap/lib/screens.dart/Sign.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignUp_Screen extends StatefulWidget {
+  const SignUp_Screen({super.key});
+
+  @override
+  State<SignUp_Screen> createState() => _SignUp_ScreenState();
+}
+
+class _SignUp_ScreenState extends State<SignUp_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
